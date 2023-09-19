384bd7ac6c8bbfb8e15dab695686d88606a20bf4
Ryan <97636691+rbass6@users.noreply.github.com>
2023-09-18 22:00:02 -0400

    Add calendar

--- DatePicker.js
+++ DatePicker.js
@@ -53,16 +53,17 @@ function numToMonth(month){
 
 function getCalendarHTML(date){
   var month = numToMonth(date.getMonth() + 1); // Add one since getMonth index starts at 0
-
+  var year = date.getFullYear();
+  // Contains sample data. Dates will need to be added programmatically.
   return `
   <table>
     <thead>
       <tr>
-        <th colspan="7">${month}</th>
+        <th colspan="7">${month} ${year}</th>
       </tr>
     </thead>
     <tbody>
-      <tr>
+      <tr id="weekday-row">
         <td>Su</td>
         <td>Mo</td>
         <td>Tu</td>
@@ -70,6 +71,51 @@ function getCalendarHTML(date){
         <td>Th</td>
         <td>Fr</td>
         <td>Sa</td>
+      </tr>
+      <tr class="date-row">
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+        <td>4</td>
+        <td>5</td>
+        <td>6</td>
+        <td>7</td>
+      </tr>
+      <tr class="date-row">
+        <td>8</td>
+        <td>9</td>
+        <td>10</td>
+        <td>11</td>
+        <td>12</td>
+        <td>13</td>
+        <td>14</td>
+      </tr>
+      <tr class="date-row">
+        <td>15</td>
+        <td>16</td>
+        <td>17</td>
+        <td>18</td>
+        <td>19</td>
+        <td>20</td>
+        <td>21</td>
+      </tr>
+      <tr class="date-row">
+        <td>22</td>
+        <td>23</td>
+        <td>24</td>
+        <td>25</td>
+        <td>26</td>
+        <td>27</td>
+        <td>28</td>
+      </tr>
+      <tr class="date-row">
+        <td>29</td>
+        <td>30</td>
+        <td>31</td>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+        <td>4</td>
       </tr>
     </tbody>
   </table>
